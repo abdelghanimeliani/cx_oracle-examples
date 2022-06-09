d41cb48613e0fb0de3d7c816a3a730e7237fea9f
abdelghani <a.meliani@esi-sba.dz>
2022-06-09 02:03:12 +0100

    first one

---
+++ plsql_func.sql
@@ -0,0 +1,8 @@
+create table ptab (mydata varchar(20), myid number);
+
+create or replace function myfunc(d_p in varchar2, i_p in number) return number as
+  begin
+    insert into ptab (mydata, myid) values (d_p, i_p);
+    return (i_p * 2);
+  end;
+/
---
+++ plsql_proc.sql
@@ -0,0 +1,5 @@
+create or replace procedure myproc(v1_p in number, v2_p out number) as
+begin
+  v2_p := v1_p * 2;
+end;
+/
